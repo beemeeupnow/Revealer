eae3ee01a69d6df66852de954fb21438f61178d3
beemeeupnow <beemeeupnow@gmail.com>
2024-03-02 08:57:34 -0700

    Update conditions.py to use Mumbai and provided type ContractConditionDict

--- revealer/conditions.py
+++ revealer/conditions.py
@@ -1,19 +1,24 @@
-from nucypher.policy.conditions.lingo import Lingo
+from nucypher.policy.conditions.lingo import ConditionLingo, ConditionType, Lingo
+from nucypher.policy.conditions.types import ContractConditionDict, ReturnValueTestDict
+
+contract_condition = ContractConditionDict(
+    name="isKeySet",
+    conditionType=ConditionType.CONTRACT.value,
+    chain=80001,
+    method="isKeySet",
+    returnValueTest=ReturnValueTestDict(comparator="==", value=True),
+    contractAddress="0x96ebdf35199219BDd16E3c3E1aD8C89C9185b734",  # set this to the contract that has initialWindow
+    standardContractType="",
+    functionAbi={
+        "inputs": [],
+        "name": "isKeySet",
+        "outputs": [{"internalType": "bool", "name": "", "type": "bool"}],
+        "stateMutability": "view",
+        "type": "function",
+    }
+)
 
 is_material_released_condition: Lingo = {
-        "version": "1.0.0",
-        "condition": {
-            "conditionType": "contract",
-            "contractAddress": "0x96ebdf35199219BDd16E3c3E1aD8C89C9185b734",  # contract with initialWindow
-            "functionAbi": {
-                "inputs": [],
-                "name": "isKeySet",
-                "outputs": [{"internalType": "bool", "name": "", "type": "bool"}],
-                "stateMutability": "view",
-                "type": "function",
-            },  # type: ignore
-            "method": "isKeySet",
-            "chain": 5,
-            "returnValueTest": {"comparator": "==", "value": True},
-        },
+        "version": ConditionLingo.VERSION,
+        "condition": contract_condition,
     }
