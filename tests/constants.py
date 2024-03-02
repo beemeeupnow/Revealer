2fbaf14eee044d7670645192354ca7aee4bab32d
beemeeupnow <beemeeupnow@gmail.com>
2024-03-02 08:59:08 -0700

    Borrow the default test key from nucypher

---
+++ tests/constants.py
@@ -0,0 +1,8 @@
+from hexbytes import HexBytes
+
+# borrowed from https://github.com/nucypher/nucypher/blob/main/tests/constants.py#L99
+
+# private key for wallet address '0x070a85eD1Ddb44ecD07e746235bE0B959ff5b30A'
+DEFAULT_TEST_ENRICO_PRIVATE_KEY = HexBytes(
+    "0x900edb9e8214b2353f82aa195e915128f419a92cfb8bbc0f4784f10ef4112b86"
+)
